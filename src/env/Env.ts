167f7f354e63e660b106bf9818eaf082f27a8e3c
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-08-01 11:22:15 -0300

    class to validate env variables

---
+++ src/env/Env.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+
+class Env {
+   NODE_ENV?: string
+   PORT?: number | string
+
+   constructor(){
+      if(this.NODE_ENV === undefined){
+         this.NODE_ENV = 'dev'
+      }
+
+      if(this.PORT === undefined){
+         this.PORT = 3333
+      }
+
+      if(typeof(this.PORT) === 'string'){
+         this.PORT = parseInt(this.PORT)
+      }
+   }
+}
+
+const _env = new Env()
+_env.PORT = process.env.PORT
+
+export {
+   _env
+} 
