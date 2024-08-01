a3f07a3ead38f94585b0190478532b9abedf4506
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-08-01 11:56:11 -0300

    eslint config applied

--- src/env/Env.ts
+++ src/env/Env.ts
@@ -1,27 +1,25 @@
 import 'dotenv/config'
 
 class Env {
-   NODE_ENV?: string
-   PORT?: number | string
+  NODE_ENV?: string
+  PORT?: number | string
 
-   constructor(){
-      if(this.NODE_ENV === undefined){
-         this.NODE_ENV = 'dev'
-      }
+  constructor() {
+    if (this.NODE_ENV === undefined) {
+      this.NODE_ENV = 'dev'
+    }
 
-      if(this.PORT === undefined){
-         this.PORT = 3333
-      }
+    if (this.PORT === undefined) {
+      this.PORT = 3333
+    }
 
-      if(typeof(this.PORT) === 'string'){
-         this.PORT = parseInt(this.PORT)
-      }
-   }
+    if (typeof this.PORT === 'string') {
+      this.PORT = parseInt(this.PORT)
+    }
+  }
 }
 
 const _env = new Env()
 _env.PORT = process.env.PORT
 
-export {
-   _env
-} 
+export { _env }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-// @ts-nocheck
+import { app } from './app'
+import { _env } from './env/Env'
 
-import { app } from "./app";
-import { _env } from "./env/Env";
-
-app.listen ({
-   host: '0.0.0.0',
-   port: _env.PORT,
-}).then(() => {
-   console.log('🚀 HTTP server runnning!')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: _env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server runnning!')
+  })
