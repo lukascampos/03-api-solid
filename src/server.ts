5c7630a04b55d5b22863522a682c44f2f8e5a9bc
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-08-01 11:23:07 -0300

    using _env object with .env variable to set port

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+// @ts-nocheck
+
 import { app } from "./app";
+import { _env } from "./env/Env";
 
 app.listen ({
    host: '0.0.0.0',
-   port: 3333,
+   port: _env.PORT,
 }).then(() => {
    console.log('🚀 HTTP server runnning!')
 })
