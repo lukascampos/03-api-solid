ba8ead9b3de5990567dc90f197e82dbf2fa89b59
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-08-05 15:54:11 -0300

    removed insert new user into database

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,40 +1,22 @@
+import { registerService } from '@/services/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
-import { User } from '@/User'
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
 export async function register(req: FastifyRequest, rep: FastifyReply) {
-   const registerBodySchema = z.object({
-      name: z.string(),
-      email: z.string().email(),
-      password: z.string().min(6),
-   })
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-   const { name, email, password } = registerBodySchema.parse(req.body)
+  const { name, email, password } = registerBodySchema.parse(req.body)
 
-   const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-         email,
-      }
-   })
+  try {
+    await registerService({ name, email, password })
+  } catch (err) {
+    return rep.status(409).send()
+  }
 
-   if(userWithSameEmail){
-      return rep.status(409).send()
-   }
-
-   const user = new User(name, email, password)
-
-   const passwordHash = await hash(user.getPassword, 6)
-
-   await prisma.user.create({
-      data: {
-         name: user.getName,
-         email: user.getEmail,
-         password: passwordHash,
-      },
-   })
-
-   return rep.status(201).send()
+  return rep.status(201).send()
 }
