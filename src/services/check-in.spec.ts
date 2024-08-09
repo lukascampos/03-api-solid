0111f981d085651c6aa2a2be2ffab057ef6d8df1
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-08-09 11:30:58 -0300

    check in service and test suit

---
+++ src/services/check-in.spec.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInService } from './check-in'
+import { randomUUID } from 'node:crypto'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInService
+
+describe('Check-in Service', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInService(checkInsRepository)
+  })
+
+  it('Should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      userId: randomUUID(),
+      gymId: randomUUID(),
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
---
+++ src/services/check-in.ts
@@ -0,0 +1,34 @@
+import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckInServiceRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      checkIn,
+    }
+  }
+}
